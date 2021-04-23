6b507a819b5a1291990e81609fb3e45634fec897
Covid Help <covidhelp.in.website@gmail.com>
2021-04-23 16:59:00 +0530

    Fixed error for created_by field.
    For the Available model, the created_by field was recently added. This
    caused adding content to fail. Fixed by updating the Views.

--- covidhelp/webapp/views.py
+++ covidhelp/webapp/views.py
@@ -69,6 +69,10 @@ class AvailableCreateView(LoginRequiredMixin, CreateView):
         "additional_text"
     ]
 
+    def form_valid (self,form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
     def get_success_url(self):
         return reverse('home')
 
