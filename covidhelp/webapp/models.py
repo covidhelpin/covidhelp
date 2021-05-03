261ed25a0470f080b324ae0c14067b761be9fd8b
Covid Help <covidhelp.in.website@gmail.com>
2021-05-03 15:50:39 +0530

    Added descriptions to the Requirements.
    This helps to add content to the pages.

--- covidhelp/webapp/models.py
+++ covidhelp/webapp/models.py
@@ -34,6 +34,7 @@ VERIFIED_CHOICES = [
 
 class Requirements(models.Model):
     name = models.CharField(max_length=30)
+    description = models.CharField(max_length=200,null=True, blank=True)
 
     def __str__(self):
         return self.name
